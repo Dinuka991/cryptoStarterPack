42c862e1ed85c92203a738f38355e07e6dd6c4eb
Dev Darshana <dinukaekanayake92@gmail.com>
2021-03-03 19:52:18 +0530

    aes and rsa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './project/home/home.component';
+import { HomeComponent } from './project/aesUtils/home.component';
 import { RSAutilsComponent } from './project/rsautils/rsautils.component';
 
 const routes: Routes = [
