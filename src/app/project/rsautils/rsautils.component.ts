e13c0167e1a883a0f3e94accfc721a25bfd290c0
Dev Darshana <dinukaekanayake92@gmail.com>
2021-03-03 19:36:52 +0530

    done and dusted

--- src/app/project/rsautils/rsautils.component.ts
+++ src/app/project/rsautils/rsautils.component.ts
@@ -20,30 +20,31 @@ export class RSAutilsComponent implements OnInit {
 
   constructor() {
     const privatekey = `-----BEGIN RSA PRIVATE KEY-----
-    MIICXQIBAAKBgQChSh7Uz5Cvuduo9fdhEvn7LWrDUZaX4biAX5kBcvrGQzCiht1O
-    zMgTjTXrlpLJfmVGojP05MFOuVzXDnjUGO0vZtkoh38QY4yjNamDcFqL7N66xNJD
-    iK6QVPV/G5h5nD9gx9gKS2GgKeaoTRk+pOS/5lP5GEubxRjz/DuKbUaAOQIDAQAB
-    AoGBAJnONNh7l137vpzA8caAciI0jTr08sajWW4Ru7bGDp5CNH7IXQJoBeOfJWwj
-    ipbbEXjp9OHzwuNvoDj0dATOQF8Id9eo7M/LI+XXrUd/4EIy+8xCjlxXBDILUoYQ
-    mMMxz+uVn0q2jXBUajcAnUY/Ys8on9XNrMDEkyAAmLepx16BAkEA94MNTOE/ytre
-    cRg+isNWOL2cJcknHDj1j8U4sTO8QfBriuqDZj0cZ+63mBEoJosL6JqoOqCBT5G8
-    DWXsJdHCPwJBAKbSG557PUuJ2cJCwV6LzBrnlBN0/qV6J7/Lqkk8J52a/si+/fUq
-    pbMtxH0buMjl5kd18m7DjmTuOprXfDAgr4cCQQCMy1+le/xtyFMAwmnRRYymejV4
-    yRQWWdBWGDpHksZkO/7w5rYYUmr2NDT7q8bLJg2AI3nO376vJJd0h6XtXYTBAkAp
-    YPR42xjZI9hf1y8fxI1cHxUbvmlromE5scsLX2B0N337TWSw62MujquuXLH5Iw17
-    rEX2M4uefXkL72deiL0BAkBWxMGXg2b1dix4IYSyxKcy1nfV0umsLJeOZ0r17+CZ
-    gSUZVruJd9YPIbNwV3rs7VZuLGScslOd2oH5j9TeXP5r
-    -----END RSA PRIVATE KEY-----`;
+MIICXQIBAAKBgQChSh7Uz5Cvuduo9fdhEvn7LWrDUZaX4biAX5kBcvrGQzCiht1O
+zMgTjTXrlpLJfmVGojP05MFOuVzXDnjUGO0vZtkoh38QY4yjNamDcFqL7N66xNJD
+iK6QVPV/G5h5nD9gx9gKS2GgKeaoTRk+pOS/5lP5GEubxRjz/DuKbUaAOQIDAQAB
+AoGBAJnONNh7l137vpzA8caAciI0jTr08sajWW4Ru7bGDp5CNH7IXQJoBeOfJWwj
+ipbbEXjp9OHzwuNvoDj0dATOQF8Id9eo7M/LI+XXrUd/4EIy+8xCjlxXBDILUoYQ
+mMMxz+uVn0q2jXBUajcAnUY/Ys8on9XNrMDEkyAAmLepx16BAkEA94MNTOE/ytre
+cRg+isNWOL2cJcknHDj1j8U4sTO8QfBriuqDZj0cZ+63mBEoJosL6JqoOqCBT5G8
+DWXsJdHCPwJBAKbSG557PUuJ2cJCwV6LzBrnlBN0/qV6J7/Lqkk8J52a/si+/fUq
+pbMtxH0buMjl5kd18m7DjmTuOprXfDAgr4cCQQCMy1+le/xtyFMAwmnRRYymejV4
+yRQWWdBWGDpHksZkO/7w5rYYUmr2NDT7q8bLJg2AI3nO376vJJd0h6XtXYTBAkAp
+YPR42xjZI9hf1y8fxI1cHxUbvmlromE5scsLX2B0N337TWSw62MujquuXLH5Iw17
+rEX2M4uefXkL72deiL0BAkBWxMGXg2b1dix4IYSyxKcy1nfV0umsLJeOZ0r17+CZ
+gSUZVruJd9YPIbNwV3rs7VZuLGScslOd2oH5j9TeXP5r
+-----END RSA PRIVATE KEY-----`;
+
+
+    const publickey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChSh7Uz5Cvuduo9fdhEvn7LWrD
+UZaX4biAX5kBcvrGQzCiht1OzMgTjTXrlpLJfmVGojP05MFOuVzXDnjUGO0vZtko
+h38QY4yjNamDcFqL7N66xNJDiK6QVPV/G5h5nD9gx9gKS2GgKeaoTRk+pOS/5lP5
+GEubxRjz/DuKbUaAOQIDAQAB
+-----END PUBLIC KEY-----`;
     
-    
-        const publickey = `-----BEGIN PUBLIC KEY-----
-    MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQChSh7Uz5Cvuduo9fdhEvn7LWrD
-    UZaX4biAX5kBcvrGQzCiht1OzMgTjTXrlpLJfmVGojP05MFOuVzXDnjUGO0vZtko
-    h38QY4yjNamDcFqL7N66xNJDiK6QVPV/G5h5nD9gx9gKS2GgKeaoTRk+pOS/5lP5
-    GEubxRjz/DuKbUaAOQIDAQAB
-    -----END PUBLIC KEY-----`;
-    
-      const decryptDataRow = `T7lL2gWfbo4vwSIl9akIhpv/+Ww7RIZA+Mb0M+Op6fey4EErkaJTUTFRy5EIka3MccFZnoP7cPDnegw2z4FgHm1oU6ytn0vg5uOz6meo8qlvnUEFmjlrBmQqGsMry+QIBT6muUZVIeq8AgANyAYL6S6T5vtHZpOPeO04Zcckpmc=`;
+        this.encrypt.setPublicKey(publickey);
+        const decryptDataRow =  this.encrypt.encrypt('saman');
         this.name = `Angular! v${VERSION.full}`
         this.decrypt.setPrivateKey(privatekey);
         this.decryptdata = this.decrypt.decrypt(decryptDataRow);
